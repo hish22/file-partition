5b583fd8fe19c2587a6271a0ae25ece649271655
hisham jog <hish522@hotmail.com>
2026-05-13 12:44:11 +0300

    refactor: seperate parition logic into function, feat: generate pmd file

--- file_part.c
+++ file_part.c
@@ -2,25 +2,58 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argv, char **argc) {
+/* pmd metadata */
+struct pmd{
+    char filename[256];
+    int parts_bound;
+    int block_size;
+    int extra_remain;
+    int file_size;
+};
 
-    // Input from user
-    if (argv != 3) {
-        printf("Please inpout 2 arguments (usage: %s filename blockpart)\n",argc[0]);
-        exit(1);
+
+/* generate pmd */
+void generate_pmd(char filename[256],int parts_bound,int block_size, 
+    int extra_remain,int file_size) {
+
+    // request new pmd block
+    struct pmd new_pmd;
+
+    // fill pmd block with data
+    strncpy(new_pmd.filename,filename,sizeof(new_pmd.filename));
+    new_pmd.parts_bound = parts_bound;
+    new_pmd.block_size = block_size;
+    new_pmd.extra_remain = extra_remain;
+    new_pmd.file_size = file_size;
+
+    printf("filename: %s \n parts_bound: %x \n block_size: %x\n file_size: %x \n",
+        filename,parts_bound,block_size,file_size);
+
+    // array of chars
+    char filename_buffer[1024];
+    sprintf(filename_buffer,"%s.pmd",filename);
+
+    // write to disk
+    FILE *fh = fopen(filename_buffer,"wb");
+    if (fh != NULL) {
+        fwrite(&new_pmd, sizeof(new_pmd), 1, fh);
+        fclose(fh);
     }
-    int block_size = atoi(argc[2]);
+}
 
+/* Partition of files into parts */
+void partition(char *_filename,int parts_number) {   
     // size vaildation
-    if (block_size > 16 || block_size <= 0) {
+    if (parts_number > 16 || parts_number <= 0) {
         printf("Please enter parts size less than 16\n");
         exit(1);
     }
 
     // file metadata
     struct stat st;
-    if (stat(argc[1],&st) == -1) {
+    if (stat(_filename,&st) == -1) {
         printf("File doesn't exist!\n");
         exit(1);
     }
@@ -30,14 +63,14 @@ int main(int argv, char **argc) {
         printf("Size of file is large!\n");
         exit(1);
     }
-    int r = (st.st_size % block_size);
-    int new_file_size = (st.st_size - r) / block_size;
+    int r = (st.st_size % parts_number);
+    int block_size = (st.st_size - r) / parts_number;
 
     // Memory allocation (allocate based on file size)
     void *ptr = malloc(st.st_size);
     
     // Open the file
-    FILE *fh = fopen(argc[1],"rb");
+    FILE *fh = fopen(_filename,"rb");
     if (fh != NULL) {
         fread(ptr, st.st_size, 1, fh);
         fclose(fh);
@@ -46,20 +79,47 @@ int main(int argv, char **argc) {
     char *fptr = (char*) ptr;
 
     // Create parts into different files
-    for(int i=0; i < block_size; i++) {
+    for(int i=0; i < parts_number; i++) {
         char filename[1024];
-        snprintf(filename, sizeof(filename), "%s.%d", argc[1], i);
+        snprintf(filename, sizeof(filename), "%s.%d", _filename, i);
         FILE *fh = fopen(filename,"wb");
         if (fh != NULL) {
-            if (i+1 == block_size) {
-                fwrite(fptr, new_file_size+r, 1, fh);
+            if (i+1 == parts_number) {
+                fwrite(fptr, block_size+r, 1, fh);
                 fclose(fh);
             } else {
-                fwrite(fptr, new_file_size, 1, fh);
+                fwrite(fptr, block_size, 1, fh);
                 fclose(fh);
             }
         }
-        fptr += new_file_size;
+        fptr += block_size;
+    }
+    generate_pmd(_filename, parts_number, block_size, r, st.st_size);
+    free(ptr);
+}
+
+int main(int argv, char **argc) {
+
+    // Input from user
+    if (argv != 4) {
+        printf("Please inpout 3 arguments (usage: %s flag filename blockpart)\n",argc[0]);
+        exit(1);
+    }
+    
+    if (argc[1][0] == '-') {
+        switch (argc[1][1])
+        {
+        case 'p':
+            partition(argc[2],atoi(argc[3]));
+            break;
+        
+        case 'm':
+        ;
+        
+        default:
+            printf("Failed to understand the flag!\n");
+            break;
+        }
     }
 
     // Create File again
@@ -78,8 +138,6 @@ int main(int argv, char **argc) {
     // }
 
     // fclose(fh_c);
-
-    free(ptr);
 
     return 0;
 
